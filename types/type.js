641bca49a88eb4c1fde2afd3e9c054315fdd5ba4
matheus-fernando <matheus.fernando.coder@gmail.com>
2022-02-03 18:03:03 -0300

    I've learned about how to declare a varible and how to tell what kind of date it is

--- types/type.js
+++ types/type.js
@@ -1,1 +1,12 @@
 "use strict";
+//Tipos
+let typeOfData1 = 15;
+let typeOfData2 = 'String';
+//Arrays
+let heroes = ['Spider Man', 'Super Man', 'Batman'];
+// console.log(typeof heroes);
+heroes.push('Captain Marvel');
+// console.log(heroes)
+//Tuplas
+let birthday = ['Algust', 21];
+console.log(typeof birthday);
---
+++ types/type.ts
@@ -0,0 +1,14 @@
+//Tipos
+let typeOfData1: number = 15;
+let typeOfData2: string = 'String';
+
+//Arrays
+let heroes: string[] = ['Spider Man', 'Super Man', 'Batman'];
+// console.log(typeof heroes);
+heroes.push('Captain Marvel');
+// console.log(heroes)
+
+//Tuplas
+let birthday: [string, number] = ['Algust' ,21 ]
+console.log(typeof birthday);
+
