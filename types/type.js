eac9df41c2d5fe64758d06611c5e8b59b9634acc
matheus-fernando <matheus.fernando.coder@gmail.com>
2022-02-05 19:03:55 -0300

    Creating functions

--- types/type.js
+++ types/type.js
@@ -1,12 +1,31 @@
 "use strict";
-//Tipos
-let typeOfData1 = 15;
-let typeOfData2 = 'String';
-//Arrays
-let heroes = ['Spider Man', 'Super Man', 'Batman'];
-// console.log(typeof heroes);
-heroes.push('Captain Marvel');
-// console.log(heroes)
-//Tuplas
-let birthday = ['Algust', 21];
-console.log(typeof birthday);
+// //Tipos
+// let typeOfData1: number = 15;
+// let typeOfData2: string = 'String';
+// //Arrays
+// let heroes: string[] = ['Spider Man', 'Super Man', 'Batman'];
+// // console.log(typeof heroes);
+// heroes.push('Captain Marvel');
+// // console.log(heroes)
+// //Tuplas
+// let birthday: [string, number] = ['Algust' ,21 ]
+// console.log(typeof birthday);
+// //Enum
+// enum Cor {
+//     gray,
+//     green,
+//     blue,
+// }
+// let minhaCor: Cor = Cor.green;
+// console.log(minhaCor)
+// //Any
+// let car: any = 'Bmw';
+let name1 = 'Juni de souza';
+function sayName() {
+    return name1;
+}
+console.log(sayName());
+function birthday(birthYear, currentYear) {
+    return currentYear - birthYear;
+}
+console.log(birthday(1996, 2022));
--- types/type.ts
+++ types/type.ts
@@ -1,14 +1,39 @@
-//Tipos
-let typeOfData1: number = 15;
-let typeOfData2: string = 'String';
+// //Tipos
+// let typeOfData1: number = 15;
+// let typeOfData2: string = 'String';
 
-//Arrays
-let heroes: string[] = ['Spider Man', 'Super Man', 'Batman'];
-// console.log(typeof heroes);
-heroes.push('Captain Marvel');
-// console.log(heroes)
+// //Arrays
+// let heroes: string[] = ['Spider Man', 'Super Man', 'Batman'];
+// // console.log(typeof heroes);
+// heroes.push('Captain Marvel');
+// // console.log(heroes)
 
-//Tuplas
-let birthday: [string, number] = ['Algust' ,21 ]
-console.log(typeof birthday);
+// //Tuplas
+// let birthday: [string, number] = ['Algust' ,21 ]
+// console.log(typeof birthday);
 
+// //Enum
+// enum Cor {
+//     gray,
+//     green,
+//     blue,
+// }
+
+// let minhaCor: Cor = Cor.green;
+// console.log(minhaCor)
+
+// //Any
+// let car: any = 'Bmw';
+
+let name1 = 'Juni de souza';
+function sayName(): string {
+    return name1;
+}
+
+console.log(sayName());
+
+function birthday(birthYear:number, currentYear: number): number {
+    return currentYear - birthYear;
+}
+
+console.log(birthday(1996,2022));
